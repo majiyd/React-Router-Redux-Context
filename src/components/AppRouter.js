8785eacfe442f4ab7b1873de132b81ca96fd1abe
majiydm <suleimanmajiyd@gmail.com>
2019-04-20 00:28:02 +0100

    ran create-react-app and installed react router

---
+++ src/components/AppRouter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import Home from './Home'
+import About from "./About.jsx";
+import Contact from "./Contact";
+import Users from "./Users";
+
+export default class AppRouter extends React.Component{
+    render(){
+        return (
+			<Router>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/About/">About Us</Link>
+				</li>
+				<li>
+					<Link to="/Contact/">Contact Us</Link>
+				</li>
+				<li>
+					<Link to="/Users/">Users</Link>
+				</li>
+
+				<Route path="/" exact component={Home} />
+				<Route path="/About/" component={About} />
+				<Route path="/Contact/" component={Contact} />
+				<Route path="/Users/" component={Users} />
+			</Router>
+		);
+    }
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Contact extends React.Component{
+    render(){
+        return (
+			<React.Fragment>
+				<h1>Contact</h1>
+			</React.Fragment>
+		);
+    }
+}
