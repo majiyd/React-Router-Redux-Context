9349e623f88d6b3fdb1f0a1b1f292772cad40dfc
majiyd <suleimanmajiyd@gmail.com>
2019-04-20 13:07:02 +0100

    did a bit of react router styling

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
-import Home from './Home'
+import React from "react";
+import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
+import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Users from "./Users";
-import NotFound from "./404.js"
+import NotFound from "./404";
+import styles from "../styles/Nav.module.css";
 
-export default class AppRouter extends React.Component{
-    render(){
-        return (
-			<Router>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/About/">About Us</Link>
-				</li>
-				<li>
-					<Link to="/Contact/">Contact Us</Link>
-				</li>
-				<li>
-					<Link to="/Users/">Users</Link>
-				</li>
+export default class AppRouter extends React.Component {
+  render() {
+    return (
+      <Router >
+				<nav className={styles.nav}>
+					<li className={styles.link}>
+						<NavLink activeClassName="active" to="/">Home</NavLink>
+					</li>
+					<li className={styles.link}>
+						<NavLink activeClassName="active" to="/About/">About Us</NavLink>
+					</li>
+					<li className={styles.link}>
+						<NavLink activeClassName="active" to="/Contact/">Contact Us</NavLink>
+					</li>
+					<li className={styles.link}>
+						<NavLink activeClassName="active" to="/Users/">Users</NavLink>
+					</li>
+				</nav>
 
-				<Switch>	
-					<Route path="/" exact component={Home} />
-					<Route path="/About/" component={About} />
-					<Route path="/Contact/" component={Contact} />
-					<Route path="/Users/:id" component={Users} />
-					<Route component={NotFound} />
-				</Switch>
-			</Router>
-		);
-    }
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/About/" component={About} />
+          <Route path="/Contact/" component={Contact} />
+          <Route path="/Users/:id" component={Users} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
